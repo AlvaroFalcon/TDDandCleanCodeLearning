8c67de68405e8d2a9632fcb85f4fac36119e24a5
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-05-27 21:27:17 +0100

    Vasya-Clerk kata added

---
+++ src/VasyaClerkKata/VasyaClerk.java
@@ -0,0 +1,10 @@
+package VasyaClerkKata;
+
+/**
+ * Created by Alvaro on 27/05/2016.
+ */
+public class VasyaClerk {
+    public static String Tickets(int[] ints) {
+        return "";
+    }
+}
---
+++ tests/VasyaClerkTests/VasyaClerkKataTests.java
@@ -0,0 +1,18 @@
+package VasyaClerkTests;
+
+import VasyaClerkKata.VasyaClerk;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class VasyaClerkKataTests {
+    @Test
+    public void test1() {
+        assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 25, 50}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("NO", VasyaClerk.Tickets(new int[]{25, 100}));
+    }
+}
