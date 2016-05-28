9221350dac953c641adafe01f57ced2ed8bad042
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-05-28 01:59:59 +0100

    with_one_person_with_25_dollars_should_return_YES

--- src/VasyaClerkKata/VasyaClerk.java
+++ src/VasyaClerkKata/VasyaClerk.java
@@ -1,10 +1,28 @@
 package VasyaClerkKata;
 
-/**
- * Created by Alvaro on 27/05/2016.
- */
+import java.util.Arrays;
+
 public class VasyaClerk {
-    public static String Tickets(int[] ints) {
-        return "";
+    public static String Tickets(int[] peopleMoney) {
+        for (int i = 0; i < peopleMoney.length; i++) {
+            if (!gotChange(peopleMoney, i)) return "NO";
+        }
+        return "YES";
     }
+
+    private static boolean gotChange(int[] peopleMoney, int i) {
+        int[] vasyaChange = Arrays.copyOfRange(peopleMoney, 0, i);
+        if (gotChangeForFifty(vasyaChange)) return true;
+        if (gotChangeForOneHundred(vasyaChange)) return true;
+        return false;
+    }
+
+    private static boolean gotChangeForFifty(int[] vasyaChange) {
+        return false;
+    }
+
+    private static boolean gotChangeForOneHundred(int[] vasyaChange) {
+        return false;
+    }
+
 }
--- tests/VasyaClerkTests/VasyaClerkKataTests.java
+++ tests/VasyaClerkTests/VasyaClerkKataTests.java
@@ -7,6 +7,16 @@ import static junit.framework.TestCase.assertEquals;
 
 public class VasyaClerkKataTests {
     @Test
+    public void with_one_person_with_25_dollars_should_return_YES() {
+        assertEquals("YES", VasyaClerk.Tickets(new int[]{25}));
+    }
+
+    @Test
+    public void with_two_person_with_25_and_50_dollars_should_return_YES() {
+        assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 50}));
+    }
+
+    @Test
     public void test1() {
         assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 25, 50}));
     }
