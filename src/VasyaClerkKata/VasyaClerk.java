e7c682e6fd6c9921ddb9e35a1bc9a3a5e94462c5
Alvaro Falcon <alvaro.falcon101@alu.ulpgc.es>
2016-05-28 22:34:27 +0100

    All tests passed and refactored

--- src/VasyaClerkKata/VasyaClerk.java
+++ src/VasyaClerkKata/VasyaClerk.java
@@ -1,48 +1,101 @@
 package VasyaClerkKata;
 
-import java.util.Arrays;
 
 public class VasyaClerk {
-    public static String Tickets(int[] peopleMoney) {
+    private int twentyFiveTickets = 0;
+
+    private int fiftyTickets = 0;
+
+    public String Tickets(int[] peopleMoney) {
         for (int person = 0; person < peopleMoney.length; person++) {
-            if (!gotChange(peopleMoney)) return "NO";
+            if (!gotChange(peopleMoney[person])) return "NO";
         }
         return "YES";
     }
 
-    private static boolean gotChange(int[] peopleMoney) {
-        int numberOfTwentyFive = countTicket(peopleMoney, 25);
-        int numberOfFifty = countTicket(peopleMoney, 50);
-        int numberOfOneHundred = countTicket(peopleMoney, 100);
-        return checkChange(numberOfTwentyFive, numberOfFifty, numberOfOneHundred);
+    private boolean gotChange(int peopleMoney) {
+        updateTickets(peopleMoney);
+        return isPossiblyToChange(peopleMoney);
     }
 
-    private static boolean checkChange(int numberOfTwentyFive, int numberOfFifty, int numberOfOneHundred) {
-        if (checkFiftyChange(numberOfTwentyFive, numberOfFifty) && checkOneHundredChange(numberOfTwentyFive, numberOfFifty, numberOfOneHundred))
-            return true;
+    private void updateTickets(int peopleMoney) {
+        if (peopleMoney == 25) addTwentyFiveTicket();
+        if (peopleMoney == 50) addFiftyTicket();
+
+    }
+
+    private void addTwentyFiveTicket() {
+        this.setTwentyFiveTickets(this.getTwentyFiveTickets() + 1);
+    }
+    private void addFiftyTicket() {
+        this.setFiftyTickets(this.getFiftyTickets() + 1);
+    }
+
+    private boolean isPossiblyToChange(int peopleMoney) {
+
+        if (peopleMoney == 50) return gotChangeForFifty();
+        if (peopleMoney == 100) return gotChangeForOneHundred();
+        return true;
+    }
+
+    private boolean gotChangeForOneHundred() {
+        if (gotEnoughTwentyFiveTickets(2)) return makeChangeWithTwentyFive(3);
+
+        if (gotEnoughTwentyFiveTickets(0) && gotEnoughFiftyTickets(0)) return makeChangeWithTwentyFiveAndFifty();
+
         return false;
     }
 
-    private static boolean checkOneHundredChange(int numberOfTwentyFive, int numberOfFifty, int numberOfOneHundred) {
-        if (numberOfOneHundred == 0) return true;
-        if (numberOfTwentyFive >= 3 * numberOfOneHundred) return true;
-        if ((numberOfTwentyFive >= numberOfOneHundred) && (numberOfFifty >= numberOfOneHundred)) return true;
+    private boolean gotEnoughFiftyTickets(int amount) {
+        return this.getFiftyTickets() > amount;
+    }
 
+    private boolean gotEnoughTwentyFiveTickets(int amount) {
+        return this.getTwentyFiveTickets() > amount;
+    }
+
+    private boolean makeChangeWithTwentyFiveAndFifty() {
+        reduceTwentyFiveTickets(1);
+        reduceFiftyTickets(1);
+        return true;
+    }
+
+    private void reduceFiftyTickets(int amount) {
+        this.setFiftyTickets(this.getFiftyTickets() - amount);
+    }
+
+    private boolean gotChangeForFifty() {
+        if (this.getTwentyFiveTickets() > 0) return makeChangeWithTwentyFive(1);
         return false;
     }
 
-    private static boolean checkFiftyChange(int numberOfTwentyFive, int numberOfFifty) {
-        if (numberOfFifty == 0) return true;
-        if (numberOfTwentyFive >= numberOfFifty) return true;
-        return false;
+    private boolean makeChangeWithTwentyFive(int amount) {
+        reduceTwentyFiveTickets(amount);
+        return true;
     }
 
-    private static int countTicket(int[] peopleMoney, int ticket) {
-        int ticketCount = 0;
-        for (int j = 0; j < peopleMoney.length; j++) {
-            if (peopleMoney[j] == ticket) ticketCount++;
-        }
-        return ticketCount;
+    private void reduceTwentyFiveTickets(int amount) {
+        this.setTwentyFiveTickets(this.getTwentyFiveTickets() - amount);
+    }
+
+
+
+
+
+    public int getFiftyTickets() {
+        return fiftyTickets;
+    }
+
+    public void setFiftyTickets(int fiftyTickets) {
+        this.fiftyTickets = fiftyTickets;
+    }
+
+    public int getTwentyFiveTickets() {
+        return twentyFiveTickets;
+    }
+
+    public void setTwentyFiveTickets(int twentyFiveTickets) {
+        this.twentyFiveTickets = twentyFiveTickets;
     }
 
 
--- tests/VasyaClerkTests/VasyaClerkKataTests.java
+++ tests/VasyaClerkTests/VasyaClerkKataTests.java
@@ -8,22 +8,21 @@ import static junit.framework.TestCase.assertEquals;
 public class VasyaClerkKataTests {
     @Test
     public void with_one_person_with_25_dollars_should_return_YES() {
-        assertEquals("YES", VasyaClerk.Tickets(new int[]{25}));
+        assertEquals("YES", new VasyaClerk().Tickets(new int[]{25}));
     }
 
     @Test
     public void with_two_person_with_25_and_50_dollars_should_return_YES() {
-        assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 50}));
-        assertEquals("NO", VasyaClerk.Tickets(new int[]{25, 50, 50, 50, 50}));
+        assertEquals("YES", new VasyaClerk().Tickets(new int[]{25, 50}));
     }
 
     @Test
-    public void test1() {
-        assertEquals("YES", VasyaClerk.Tickets(new int[]{25, 25, 50}));
+    public void when_25_25_50_100_should_return_YES(){
+        assertEquals("YES", new VasyaClerk().Tickets(new int[]{25, 25, 50, 100}));
     }
 
     @Test
-    public void test2() {
-        assertEquals("NO", VasyaClerk.Tickets(new int[]{25, 100}));
+    public void when_25_100_should_return_NO() {
+        assertEquals("NO", new VasyaClerk().Tickets(new int[]{25, 100}));
     }
 }
